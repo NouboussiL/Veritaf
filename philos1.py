190541fe5c7e2828a2a9ba9b1befddb77bc55762
UzayUmutSAG <uzayumut.sag@gmail.com>
2019-12-10 10:22:36 +0100

    debut philo1.py

--- philos1.py
+++ philos1.py
@@ -5,9 +5,31 @@ def Philo():
 	res+="	etat=3;\n"
 	res+="	init=0;\n"
 	res+="	0=pense;\n"
-	res+="	1=mange;\n"
-	res+="	2=faim;\n"
-	res+="	0->1 [mange];\n"
-	res+="	1->0 [finManger];\n"
+	res+="	1=faim;\n"
+	res+="	2=mange;\n"
+	res+="	0->1 [afaim];\n"
+	res+="	2->0 [finManger];\n"
+	res+="	1->2 [commManger];\n"
+	res+="	0->0 [repense];\n"
+	res+="	1->1 [tjrFaim];\n"
+	res+="	2->2 [remange];\n"
 	res+="};;\n"
 	return res
+
+def Arete():
+	res=""
+	res+="Arete="
+	res+="[nonO,droite,gauche]{\n"
+	res+="	etat=3;\n"
+	res+="	init=0;\n"
+	res+="	0=nonO;\n"
+	res+="	1=droite;\n"
+	res+="	2=gauche;\n"
+	res+="	0->1 [gd];\n"
+	res+="	1->0 [reset];\n"
+	res+="	0->2 [dg];\n"
+	res+="	2->0 [reset];\n"
+	res+="};;\n"
+	return res
+
+def solution1(n):
